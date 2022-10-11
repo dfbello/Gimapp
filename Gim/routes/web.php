25a04d97d0917f4a9da175530fe6ef6ef55a668c
Diego Fernando Bello <dbello@unal.edu.co>
2022-10-11 00:25:31 -0500

    Crear controlador, vista y plantilla modelo

--- Gim/routes/web.php
+++ Gim/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\groupClassController;
+
 
 Route::get('/', function () {
     return view('home');
@@ -10,3 +12,4 @@ Route::get('/', function () {
 
 Route::get('/login', [SessionsController::class, 'create']) -> name('login.index');
 Route::get('/register', [RegisterController::class, 'create']) -> name('register.index');
+Route::resource('/group_class', groupClassController::class);
