6f2453e9dc60c6abe09bda70a1186062996feaea
cristiansantos25 <62475238+cristiansantos25@users.noreply.github.com>
2022-10-10 20:21:42 -0500

    Tabla asignados
    Estructura
    Clave_RutinaFK1 int,
    Clave_EjercicioFK2 int,
    constraint fk_fRutinaFK1 foreign key(Clave_RutinaFK1) references Rutina(Clave_Rutina),
    constraint fk_fCEjercicioFK2 foreign key(Clave_EjercicioFK2) references Ejercicio(Clave_Ejercicio)

---
+++ Gim/database/migrations/2022_09_25_065950_create_asignados_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAsignadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asignados', function (Blueprint $table) {
+            $table->integer('Clave_RutinaFK1')->nullable()->index('fk_fRutinaFK1');
+            $table->integer('Clave_EjercicioFK2')->nullable()->index('fk_fCEjercicioFK2');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asignados');
+    }
+}
