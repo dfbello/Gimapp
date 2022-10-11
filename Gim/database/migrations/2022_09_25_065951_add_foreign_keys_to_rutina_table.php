f515aa22aa27894a23f7f5d0dc50ff993610b34c
cristiansantos25 <62475238+cristiansantos25@users.noreply.github.com>
2022-10-10 20:24:55 -0500

    Claves foraneas

---
+++ Gim/database/migrations/2022_09_25_065951_add_foreign_keys_to_rutina_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeysToRutinaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rutina', function (Blueprint $table) {
+            $table->foreign(['Clave_ClienteFK1'], 'fk_fClienteFK1')->references(['Clave_Cliente'])->on('cliente');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rutina', function (Blueprint $table) {
+            $table->dropForeign('fk_fClienteFK1');
+        });
+    }
+}
