0aa9c35187884f6a49d2bb0d1ce0fbb46005f338
cristiansantos25 <62475238+cristiansantos25@users.noreply.github.com>
2022-10-10 20:17:13 -0500

    Tabla entrenador
    Estructura
    Clave_Entrenador int,
    Nombre_Entrenador varchar(100),
    Telefono_Entrenador bigint,
    Direccion_Entrenador varchar(200),
    Correo_Entrenador varchar(50),
    Edad_Entrenador varchar(200),
    Contrasenia_Entrenador varchar(200),
    Descripcion_Entrenador varchar(200),
    Horario_Entrenador varchar(200),

---
+++ Gim/database/migrations/2022_09_25_065950_create_entrenador_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntrenadorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entrenador', function (Blueprint $table) {
+            $table->integer('Clave_Entrenador')->primary();
+            $table->string('Nombre_Entrenador', 100)->nullable();
+            $table->bigInteger('Telefono_Entrenador')->nullable();
+            $table->string('Direccion_Entrenador', 200)->nullable();
+            $table->string('Correo_Entrenador', 50)->nullable();
+            $table->string('Edad_Entrenador', 200)->nullable();
+            $table->string('Contrasenia_Entrenador', 200)->nullable();
+            $table->string('Descripcion_Entrenador', 200)->nullable();
+            $table->string('Horario_Entrenador', 200)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entrenador');
+    }
+}
