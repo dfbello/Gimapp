3d9bf6381ff8d2da9d0e643f8f4307ce4a92be4f
Diego Fernando Bello <dbello@unal.edu.co>
2022-10-28 18:58:40 -0500

    Index muestra Entrenador asociado y Register no borra los campos si hay un dato invalido

--- Gim/app/Models/Clase.php
+++ Gim/app/Models/Clase.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Clase extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the entrenador that owns the Clase
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function entrenador(){
+
+        return $this->belongsTo(Entrenador::class, 'Clave_EntrenadorFK1', 'Clave_Entrenador');
+    }
+    
 }
+
--- Gim/resources/views/GroupClass/ClassRegister.blade.php
+++ Gim/resources/views/GroupClass/ClassRegister.blade.php
@@ -9,32 +9,32 @@
         <h1 class="h3 mb-3 fw-normal">Registrar Clase Grupal</h1>
 
         <div class="form-floating w-50 m-auto">
-            <input type="text" class="form-control w-" id="nombre" name = "nombre"  placeholder="">
+            <input type="text" class="form-control w-" id="nombre" name = "nombre"  value = "{{old('nombre')}}" placeholder="">
             <label for="nombre"> Nombre de la clase</label>
         </div>
 
         <div class="form-floating w-50 m-auto">
-            <input type="text" class="form-control w-" id="descripcion" name = "descripcion" placeholder="">
+            <input type="text" class="form-control w-" id="descripcion" name = "descripcion" value = "{{old('descripcion')}}" placeholder="">
             <label for="descripcion"> Descripción </label>
         </div>
 
         <div class="form-floating w-50 m-auto">
-            <input type="number" class="form-control w-" id="cupos" name = "cupos" placeholder="ingrese un correo">
+            <input type="number" class="form-control w-" id="cupos" name = "cupos" value = "{{old('cupos')}}" placeholder="ingrese un correo">
             <label for="cupos">Cupos disponibles</label>
         </div>
 
         <div class="form-floating w-50 m-auto">
-            <input type="datetime-local" class="form-control w-" id="datetime" name = "datetime" placeholder="ingrese un correo">
+            <input type="datetime-local" class="form-control w-" id="datetime" name = "datetime" value = "{{old('datetime')}}" placeholder="ingrese un correo">
             <label for="datetime">Fecha y Hora de inicio</label>
         </div>
 
         <div class="form-floating w-50 m-auto">
-            <input type="number" class="form-control w-" id="duracion" name = "duracion" placeholder="ingrese un correo">
+            <input type="number" class="form-control w-" id="duracion" name = "duracion" value = "{{old('duracion')}}" placeholder="ingrese un correo">
             <label for="duracion">Duracion (minutos)</label>
         </div>
 
         <div class="form-floating w-50 m-auto">
-            <select name="coach" id="coach" class= "form-control w-">
+            <select name="coach" id="coach" value = "{{old('coach')}}" class= "form-control w-">
                 @foreach($Entrenadores as $entrenador)
 
                 <option value="{{$entrenador -> Clave_Entrenador}}">{{$entrenador -> Nombre_Entrenador}}</option>
