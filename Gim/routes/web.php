cbc9db01323bebabb3f3f358512af8eb43c42588
David Arias <daaariaspa@unal.edu.co>
2022-11-21 15:59:05 -0500

    inicio de creacion de roles y permisos

--- Gim/routes/web.php
+++ Gim/routes/web.php
@@ -27,6 +27,6 @@ Route::resource('/entrenamiento', entrenamientoController::class);
 Route::resource('/rutinas', rutinaController::class);
 Route::resource('/recursos', recursoController::class);
 Route::resource('/group_class', groupClassController::class);
-Route::post('/group_class/{id}/inscribirse',[groupClassController::class,'inscribirCLiente']);
+Route::post('/group_class/{id}/inscribirse',[groupClassController::class,'inscribirCliente']);
 Route::get('/cliente/{id}/asignarEntrenamiento',[ClienteController::class,'asignarEntrenamiento']);
 
